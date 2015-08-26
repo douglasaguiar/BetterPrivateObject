d1e0002cc003fd84ccabcc32cb04d94c24c17f73
CBA <Christian.Bang@mpro.de>
2015-08-26 16:45:13 +0200

    Added PrivateType support.

---
+++ BetterPrivateObject.Tests/PrivateTypeTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BetterPrivateObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterPrivateObject.Tests
+{
+  [TestClass]
+  public class PrivateTypeTests
+  {
+    [TestMethod]
+    public void InvokePrivateMethodThatReturnsBoolean()
+    {
+      dynamic subjectPO = new PrivateType<Subject>();
+
+      bool result = subjectPO.privateStaticMethodThatResturnsBoolean();
+
+      Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void InvokePrivateMethodThatReturnsBooleanWithParameter()
+    {
+      dynamic subjectPO = new PrivateType<Subject>();
+
+      bool result = subjectPO.privateStaticMethodThatReturnsBooleanWithParameter(true);
+
+      Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void InvokePrivateVoidMethod()
+    {
+      dynamic subjectPO = new PrivateType<Subject>();
+
+      subjectPO.privateStaticVoidMethod();
+
+      Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void InvokePrivateVoidMethodWithParameter()
+    {
+      dynamic subjectPO = new PrivateType<Subject>();
+
+      subjectPO.privateStaticVoidMethodWithParameter(1);
+
+      Assert.IsTrue(true);
+    }
+
+    public class Subject
+    {
+      private static bool privateStaticMethodThatResturnsBoolean() { return true; }
+      private static bool privateStaticMethodThatReturnsBooleanWithParameter(bool p1) { return p1; }
+      private static void privateStaticVoidMethod() { }
+      private static void privateStaticVoidMethodWithParameter(int p1) { }
+    }
+  }
+}
---
+++ BetterPrivateObject/PrivateType.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace BetterPrivateObject
+{
+  /// <summary>
+  /// Strongly typed and dynamic implementation of the PrivateType class
+  /// from MsTest.
+  /// </summary>
+  /// <typeparam name="T"></typeparam>
+  public class PrivateType<T> : DynamicObject
+  {
+    public PrivateType()
+    {
+    }
+
+    public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result)
+    {
+      MethodInfo method = typeof(T).GetMethod(binder.Name, BindingFlags.NonPublic | BindingFlags.Static);
+
+      if (method == null)
+      {
+        result = null;
+        return false;
+      }
+
+      result = method.Invoke(null, args);
+      return true;
+    }
+  }
+}
