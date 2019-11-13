eb075f04dd86066272777ac5ca131139ef47ffa0
Douglas Aguiar <douglas@dreamdev.io>
2019-11-13 15:45:21 +0000

    supporting .netstandard

--- src/BetterPrivateObject/PrivateObject.cs
+++ src/BetterPrivateObject/PrivateObject.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Dynamic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BetterPrivateObject
 {
